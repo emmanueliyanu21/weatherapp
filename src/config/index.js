4e1977bad13aeb623802c65589c752284b7f8e62
emmanueliyanu21 <emmanueliyanu2012@gmail.com>
2023-11-19 16:24:53 +0100

    secret api key on server

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
-    apiKey: '48eed90de9857ae5776650ffda17296a',
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
     units: 'metric',
   };
   
